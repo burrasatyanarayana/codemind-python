c7ee6e8101a798963c25a3bdf55ab3a2f93ea770
burrasatyanarayana <115419459+burrasatyanarayana@users.noreply.github.com>
2022-12-02 17:49:42 +0530

    n=int(input()) d=0 for i in range(1,n+1):     val=i+n+d     d-=2     for j in range(1,val):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,8 @@
 n=int(input())
-
+d=0
 for i in range(1,n+1):
-     
-    for j in range(1,i+1):
-        print(j,end="")
+    val=i+n+d
+    d-=2
+    for j in range(1,val):
+        print(j,end='')
     print()
