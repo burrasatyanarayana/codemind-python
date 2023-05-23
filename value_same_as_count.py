34f9385deca0dfd6329ff9611b44ae2e52d353cd
burrasatyanarayana <115419459+burrasatyanarayana@users.noreply.github.com>
2023-05-23 19:52:14 +0530

    n=int(input()) m=list(map(int,input().split())) o=0 h=0 s=[] for i in range(len(m)):     if m[i]==m.count(m[i]):         if m[i] not in s:             s.append(m[i])             h+=1
    print(h)

---
+++ value_same_as_count.py
@@ -0,0 +1,12 @@
+n=int(input())
+m=list(map(int,input().split()))
+o=0
+h=0
+s=[]
+for i in range(len(m)):
+    if m[i]==m.count(m[i]):
+        if m[i] not in s:
+            s.append(m[i])
+            h+=1
+        
+print(h)
