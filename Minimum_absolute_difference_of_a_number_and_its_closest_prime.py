fee32297ac1724dab41fc2e2d3e39232a8baba2d
burrasatyanarayana <115419459+burrasatyanarayana@users.noreply.github.com>
2023-05-30 21:19:30 +0530

    import math def prime(j):     z=int(math.sqrt(j))     for i in range(2,z+1):         if j%i==0:             return False     return True
    k=int(input())
    for i in range(k,1,-1):
        s=prime(i)
        if s==True:
            z=i
            break
    for m in range(k,k+10000,1):
        p=prime(m)
        if p==True:
            h=m
            break
    a1=abs(k-z)
    a2=abs(k-h)
    l=[]
    if a2<a1:
        print(a2)
    else:
        print(a1)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,28 @@
+import math
+def prime(j):
+    z=int(math.sqrt(j))
+    for i in range(2,z+1):
+        if j%i==0:
+            return False
+    return True
+     
+k=int(input())
+for i in range(k,1,-1):
+    s=prime(i)
+    if s==True:
+        z=i
+        break
+for m in range(k,k+10000,1):
+    p=prime(m)
+    if p==True:
+        h=m
+        break
+a1=abs(k-z)
+a2=abs(k-h)
+l=[]
+if a2<a1:
+    print(a2)
+else:
+    print(a1)
+        
+
