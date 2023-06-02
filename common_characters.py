c5407e2079fced25f51b8730c7a4305d6ad2c37e
burrasatyanarayana <115419459+burrasatyanarayana@users.noreply.github.com>
2023-06-02 21:51:45 +0530

    n=set(input().lower().split()) m=set(input().lower().split()) z=''.join(m) s=''.join(n) k='' for i in z:     if i  in s:         if i not in k:            k+=i for i in s:     if i  in z:         if i not in k:             k+=i a=list(k) a.sort() d=''.join(a) if len(k)==0:     print(-1) else:     print(d)

---
+++ common_characters.py
@@ -0,0 +1,20 @@
+n=set(input().lower().split())
+m=set(input().lower().split())
+z=''.join(m)
+s=''.join(n)
+k=''
+for i in z:
+    if i  in s:
+        if i not in k:
+           k+=i
+for i in s:
+    if i  in z:
+        if i not in k:
+            k+=i
+a=list(k)
+a.sort()
+d=''.join(a)
+if len(k)==0:
+    print(-1)
+else:
+    print(d)
