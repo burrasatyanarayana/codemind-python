db2667baabe14339a42e11f220a4b869548ca88f
burrasatyanarayana <115419459+burrasatyanarayana@users.noreply.github.com>
2023-06-02 21:49:45 +0530

    n=set(input().lower().split()) m=set(input().lower().split()) z=''.join(m) s=''.join(n) k='' for i in z:     if i not in s:         if i not in k:            k+=i for i in s:     if i not in z:         if i not in k:             k+=i a=list(k) a.sort() d=''.join(a) print(d)

---
+++ non_common_characters.py
@@ -0,0 +1,17 @@
+n=set(input().lower().split())
+m=set(input().lower().split())
+z=''.join(m)
+s=''.join(n)
+k=''
+for i in z:
+    if i not in s:
+        if i not in k:
+           k+=i
+for i in s:
+    if i not in z:
+        if i not in k:
+            k+=i
+a=list(k)
+a.sort()
+d=''.join(a)
+print(d)
