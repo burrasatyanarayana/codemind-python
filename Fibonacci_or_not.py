c35abdb332d4f287a62ba5cf1a237fb42e1c88f5
burrasatyanarayana <115419459+burrasatyanarayana@users.noreply.github.com>
2023-06-03 17:56:07 +0530

    n=int(input()) a=0 b=1 k=0 for i in range (n+2):     c=a+b     a,b=b,c     if c>n:         break     if c==n:         k=1         break if k==1:     print(True) else:     print(False)

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,27 +1,16 @@
-def dead(n):
-    if n==1:
-        return True
-    else:
-        l=[]
-        a=0
-        b=1
-        k=0
-        while True:
-            k+=1
-            if k==n:
-                break
-            
-            c=a+b
-            l.append(c)
-            a,b=b,c
-        for i in l:
-            if i==n:
-                return True
-    return False
 n=int(input())
-v=n
-x=dead(n)
-print(x)
-
-
-    
+a=0
+b=1
+k=0
+for i in range (n+2):
+    c=a+b
+    a,b=b,c
+    if c>n:
+        break
+    if c==n:
+        k=1
+        break
+if k==1:
+    print(True)
+else:
+    print(False)
